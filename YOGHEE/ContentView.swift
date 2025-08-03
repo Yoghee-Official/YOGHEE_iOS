34fdde86fb22fd609fa43db16fd1de5c3377fe57
0ofKim <pos00042@naver.com>
2025-08-03 16:55:17 +0900

    first commit

---
+++ YOGHEE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YOGHEE
+//
+//  Created by 0ofKim on 8/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YOGHEE/YOGHEEApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOGHEEApp.swift
+//  YOGHEE
+//
+//  Created by 0ofKim on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct YOGHEEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
