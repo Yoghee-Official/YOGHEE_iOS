f88519c68c53a593b3a4f94a9051ad3e1a627cf4
0ofKim <pos00042@naver.com>
2025-08-15 21:49:12 +0900

    스플래시 뷰 개발

--- YOGHEE/ContentView.swift
+++ YOGHEE/ContentView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingSplash = true
+    
     var body: some View {
-        HomeView()
+        ZStack {
+            if isShowingSplash {
+                SplashView()
+                    .transition(.opacity)
+            } else {
+                HomeView()
+                    .transition(.opacity)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                withAnimation(.easeInOut(duration: 0.5)) {
+                    isShowingSplash = false
+                }
+            }
+        }
     }
 }
 
