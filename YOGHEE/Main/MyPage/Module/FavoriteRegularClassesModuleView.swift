dcfa198abb735f0e8c1a126c3e5a50c775aa39a3
0ofKim <pos00042@naver.com>
2026-01-20 23:52:48 +0900

    달력 개발 & 그 외 간단 모듈 TODO: 달력 뷰 공통화

---
+++ YOGHEE/Main/MyPage/Module/FavoriteRegularClassesModuleView.swift
@@ -0,0 +1,32 @@
+//
+//  FavoriteRegularClassesModuleView.swift
+//  YOGHEE
+//
+//  Created by 0ofKim on 12/21/25.
+//
+
+import SwiftUI
+
+struct FavoriteRegularClassesModuleView: View {
+    let classes: [FavoriteRegularClassDTO]
+    let onItemTap: (String) -> Void
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 12.ratio()) {
+                ForEach(classes, id: \.classId) { item in
+                    FavoriteRegularClassItemView(item: item, onTap: { onItemTap(item.classId) })
+                }
+            }
+            .padding(.horizontal, 16.ratio())
+        }
+        .padding(.vertical, 12.ratio())
+    }
+}
+
+#Preview {
+    FavoriteRegularClassesModuleView(
+        classes: [],
+        onItemTap: { _ in }
+    )
+}
