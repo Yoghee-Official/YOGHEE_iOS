9cf86ff58e197671bd5da1e1cd6428b8f1b1c5e7
0ofKim <pos00042@naver.com>
2026-02-25 23:42:00 +0900

    CategoryDTO의 type 필드 삭제

--- YOGHEE/Main/Home/Module/ItemView/CategoryItemView.swift
+++ YOGHEE/Main/Home/Module/ItemView/CategoryItemView.swift
@@ -51,21 +51,21 @@ struct CategoryItemView: View {
 #Preview {
     HStack {
         CategoryItemView(
-            category: CategoryDTO(categoryId: "1", name: "서울", mainDisplay: "Y", type: .regular),
+            category: CategoryDTO(categoryId: "1", name: "서울", mainDisplay: "Y"),
             isFirst: true,
             isWide: false,
             onTap: {}
         )
         
         CategoryItemView(
-            category: CategoryDTO(categoryId: "2", name: "경기도", mainDisplay: "Y", type: .regular),
+            category: CategoryDTO(categoryId: "2", name: "경기도", mainDisplay: "Y"),
             isFirst: false,
             isWide: false,
             onTap: {}
         )
         
         CategoryItemView(
-            category: CategoryDTO(categoryId: "3", name: "이색요가", mainDisplay: "Y", type: .oneDay),
+            category: CategoryDTO(categoryId: "3", name: "이색요가", mainDisplay: "Y"),
             isFirst: false,
             isWide: true,
             onTap: {}
